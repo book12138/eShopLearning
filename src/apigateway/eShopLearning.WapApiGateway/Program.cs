dd0a62853c6f0d76627420a258cfe91fde3dfc32
shapman <book12138@outlook.com>
2021-04-17 22:33:31 +0800

    wap网关中配置日志

--- src/apigateway/eShopLearning.WapApiGateway/Program.cs
+++ src/apigateway/eShopLearning.WapApiGateway/Program.cs
@@ -1,31 +1,45 @@
+using eShopLearning.WapApiGateway;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
+using System.IO;
 
-namespace eShopLearning.WapApiGateway
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+string _namespace = typeof(Startup).Namespace;
+string _appName = _namespace.Substring(_namespace.LastIndexOf('.', _namespace.LastIndexOf('.') - 1) + 1);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureAppConfiguration((hostingContext, builder) =>
-                {
-                    builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                        .AddJsonFile("ocelot.json");
-                });
-    }
-}
+#region 定义 IConfiguration 实例获取方法
+IConfiguration GetConfiguration()
+    => (new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables()).Build();
+#endregion
+
+#region 定义 serilog
+Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+    => new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .Enrich.WithProperty("ApplicationContext", _appName)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Http(configuration["Serilog:LogstashgUrl"] ?? "http://localhost:8080") // ELK + Serilog 实现日志中心
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+#endregion
+
+var configuration = GetConfiguration();
+Log.Logger = CreateSerilogLogger(configuration);
+IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .CaptureStartupErrors(false)
+        .ConfigureAppConfiguration(x => x.AddConfiguration(configuration).AddJsonFile("ocelot.json")) // 添加 ocelot 配置
+        .UseStartup<Startup>()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseSerilog()
+        .Build();
+
+var host = BuildWebHost(configuration, args);
+
+host.Run(); // 启动应用
--- src/apigateway/eShopLearning.WapApiGateway/Startup.cs
+++ src/apigateway/eShopLearning.WapApiGateway/Startup.cs
@@ -1,15 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Serilog;
 
 namespace eShopLearning.WapApiGateway
 {
     public class Startup
     {
+        public Startup(IWebHostEnvironment env)
+        {
+            Log.Logger.Information("当前应用环境为：" + env.EnvironmentName);
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,11 +25,6 @@ namespace eShopLearning.WapApiGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseOcelot().Wait();
         }
     }
