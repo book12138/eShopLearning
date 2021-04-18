07cd2f104dc56e44b36bcf67e6b78fcb2bb44f57
shapman <book12138@outlook.com>
2021-04-18 17:57:29 +0800

    添加手机端的授权认证中心

---
+++ src/authServer/eShopLearning.MobileAuthServer/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using eShopLearning.MobileAuthServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopLearning.MobileAuthServer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Register() => View();
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            return Content(registerViewModel.Username + "_" + registerViewModel.Password + "_" + registerViewModel.PasswordAgain);
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            return Content(loginViewModel.Username + "_" + loginViewModel.Password);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
